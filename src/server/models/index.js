49c13fcc602766e7872b161a443e2a44e95c34fb
Kevin Isom <kevin.isom@gmail.com>
2019-10-23 07:39:49 +1300

    export models

--- src/server/models/index.js
+++ src/server/models/index.js
@@ -1,11 +1,18 @@
 const bookshelf = require('../bookshelf')
 const User = require('./user')
+const League = require('./league')
 
 const Users = bookshelf.Collection.extend({
   model: User,
 })
 
+const Leagues = bookshelf.Collection.extend({
+  model: League,
+})
+
 module.exports = {
+  League,
+  Leagues,
   User,
   Users,
 }
